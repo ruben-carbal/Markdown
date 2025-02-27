8bfb1ed4ab947b391652acc90f58933fe80c13dd
Ruben <rbenbal43@gmail.com>
2025-02-26 20:47:49 -0600

    index

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('pad')
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port);
