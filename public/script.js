17fa63a01cdc076559d38d0db159758f08ed2ec8
Ruben <rbenbal43@gmail.com>
2025-02-28 15:18:59 -0600

    eso es errores por todos lados

--- public/script.js
+++ public/script.js
@@ -1,17 +1,27 @@
 window.onload = function() {
     const converter = new showdown.Converter();
     const pad = document.getElementById('pad');
+
+    let previousMarkdownValue;
     let markdownArea = document.getElementById('markdown');
 
     const convertTextAreaToMarkdown = () => {
         const markdownText = pad.value;
+        previousMarkdownValue = markdownText;
         html = converter.makeHtml(markdownText);
         markdownArea.innerHTML = html;
     }
+
+    setInterval(() => {
+        if (previousMarkdownValue != pad.value) {
+            convertTextAreaToMarkdown();
+        }
+    }, 1000);
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
     sharejs.open('home', 'text', function(error, doc) {
         doc.attach_textarea(pad);
+        convertTextAreaToMarkdown();
     });
 }
